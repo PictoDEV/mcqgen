18e730375815ebcd3d8534e406ed6eca6c67d871
PictoMaster8 <atharvatheboss@outlook.com>
2024-05-30 23:12:17 +0530

    My Some stuff is done i am not using openai instead gonna use hugging face

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='Atharva',
+    author_email='atharva.goat.10@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2","huggingface_hub"],
+    packages=find_packages()
+)
